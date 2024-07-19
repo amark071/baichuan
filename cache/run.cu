26ac48da2b504d606231c217e126415982d50eaf
zhengyz <master@FORWORK>
2024-07-19 13:53:54 +0800

    update

--- cache/run.cu
+++ cache/run.cu
@@ -1,139 +1,53 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
-#include <stdio.h>
-#include <string.h>
 #include <iostream>
-#include <vector>
 
-const int kCudaThreadsNum = 512;
-inline int CudaGetBlocks(const int N)
-{
-	return (N + kCudaThreadsNum - 1) / kCudaThreadsNum;
+#define checkCudaErrors(err) { \
+    if (err != CUDA_SUCCESS) { \
+        const char *errorName; \
+        const char *errorString; \
+        cuGetErrorName(err, &errorName); \
+        cuGetErrorString(err, &errorString); \
+        std::cerr << "CUDA Error: " << errorName << ": " << errorString << std::endl; \
+        exit(EXIT_FAILURE); \
+    } \
 }
-#define CUDA_KERNEL_LOOP(i, n) for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < (n);  i += blockDim.x * gridDim.x)
-__global__ void relu_gpu(float* in, float* out, int n)
-{
-	CUDA_KERNEL_LOOP(i, n) {
-		out[i] = in[i] > 0 ? in[i] : 0;
-	}
-}
-__global__ void relu_gpu_back(float* in, float* out, int n)
-{
-	CUDA_KERNEL_LOOP(i, n) {
-		out[i] = in[i] > 0 ? 1 : 0;
-	}
-}
-__global__ void sigmoid_gpu  (float* in, float* out, int n)
-{
-	CUDA_KERNEL_LOOP(i, n) {
-		out[i] = 1 / (1 + exp(-in[i]));
-	}
-}
-__global__ void sigmoid_gpu_back(float* in, float* out, int n)
-{
-	CUDA_KERNEL_LOOP(i, n) {
-		out[i] = (1 / (1 + exp(-in[i]))) * (1 - 1 / (1 + exp(-in[i])));
-	}
-}
-void print_array(std::vector<float> arr, int num, const char* name)
-{
-	printf("\n\n%s:\n", name);
-	for (int i = 0; i < num; i++)
-	{
-		if (i % 8 == 0)
-		{
-			printf("\n");
-		}
-		printf("%.2f ", arr[i]);
-	}
-	printf("\n");
-}
-class Tensor
-{
-public:
-	std::vector<float> shape;
-	float* data;
-	bool ongpu;
-	std::vector<float> getshape()
-	{
-		return shape;
-	}
 
-	Tensor::Tensor(std::vector<float> vec, bool gpu)
-	{
-		int s = vec.size();
-		shape = vec;
-		ongpu = gpu;
-		if (ongpu)
-		{
-			cudaMalloc(&data, sizeof(float) * s);
-		}
-		else
-		{
-			data = new float[s];
-		}
-	}
-	void freecuda()
-	{
-		if (ongpu) cudaFree(data);
-		else free(data);
-	}
+int main() {
+    CUdevice device;
+    CUcontext context;
+    CUmodule module;
+    CUfunction kernel;
+    CUresult err;
 
-	Tensor cpu()
-	{
-		int s = this->shape.size();
-		std::vector<float> cpu_tensor;
-		cpu_tensor = shape;
-		cudaMemcpy(cpu_tensor.data(), this->data, s * sizeof(float), cudaMemcpyDeviceToHost);
-		Tensor temp(cpu_tensor, false);
-		return temp;
-	}
-	Tensor gpu()
-	{
-		int s = this->shape.size();
-		std::vector<float> gpu_tensor;
-		gpu_tensor = shape;
-		cudaMemcpy(gpu_tensor.data(), this->data, s * sizeof(float), cudaMemcpyHostToDevice);
-		Tensor temp(gpu_tensor, true);
-		return temp;
-	}
-};
-int main()
-{
-	//在CPU上输入一个N个元素的数组
-	const int N = 64;
-	std::vector<float> h_in;
-	h_in.reserve(N);
-	for (int i = 0; i < N; i++)
-	{
-		h_in.push_back((i - 32) * 0.1);
-	}
-	Tensor hh_in(h_in, false);
-	float* hin = (float*)malloc(N * sizeof(float));
-	float* hout = (float*)malloc(N * sizeof(float));
-	for (int i = 0; i < N; i++)
-	{
-		hin[i] = h_in[i];
-	}
-	print_array(h_in, N, "the data input on cpu");
+    // Initialize the driver API
+    err = cuInit(0);
+    checkCudaErrors(err);
 
-	Tensor dd_in = hh_in.gpu();
-	float* din;
-	float* dout;
-	cudaMalloc(&din, N * sizeof(float));
-	cudaMalloc(&dout, N * sizeof(float));
-	cudaMemcpy(din, hin, N * sizeof(float), cudaMemcpyHostToDevice);
-	//relu_gpu << <1, N >> > (din, dout, N);
-	//relu_gpu_back << <1, N >> > (din, dout, N);
-	//sigmoid_gpu << <1, N >> > (din, dout, N);
-	sigmoid_gpu_back << <1, N >> > (din, dout, N);
+    // Get the first device
+    err = cuDeviceGet(&device, 0);
+    checkCudaErrors(err);
 
-	std::vector<float> h_out;
-	h_out.resize(N);
-	cudaMemcpy(h_out.data(), dout, N * sizeof(float), cudaMemcpyDeviceToHost);
-	print_array(h_out, N, "the data output on cpu");
-	dd_in.freecuda();
-	hh_in.freecuda();
+    // Create a context
+    err = cuCtxCreate(&context, 0, device);
+    checkCudaErrors(err);
 
-	return 0;
+    // Load the module
+    err = cuModuleLoad(&module, "matmul_kernel.cubin");
+    checkCudaErrors(err);
+
+    // Get the kernel function
+    err = cuModuleGetFunction(&kernel, module, "matmul_kernel");
+    checkCudaErrors(err);
+
+    // Launch the kernel
+    void *args[] = { /* kernel arguments */ };
+    err = cuLaunchKernel(kernel, 1, 1, 1, 1, 1, 1, 0, 0, args, 0);
+    checkCudaErrors(err);
+
+    // Clean up
+    cuModuleUnload(module);
+    cuCtxDestroy(context);
+
+    return 0;
 }
